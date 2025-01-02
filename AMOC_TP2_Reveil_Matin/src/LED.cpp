047912640a4efba7aab7a5825c7c49905270a7a9
2394947 <Jennifer.m1999@outlook.fr>
2025-01-02 17:20:11 -0500

    Ajout de la classe LED.cpp

---
+++ AMOC_TP2_Reveil_Matin/src/LED.cpp
@@ -0,0 +1,16 @@
+#include "LED.h"
+#include <Arduino.h>
+
+LED::LED(const uint8_t p_pin) : m_pin(p_pin) {
+  pinMode(this->m_pin, OUTPUT);
+}
+void LED::light() const {  
+  digitalWrite(this->m_pin, HIGH);  
+}
+void LED::light(float p_intensite) const
+{
+  analogWrite(this->m_pin, (p_intensite * 255));
+}
+void LED::shut() const { 
+  digitalWrite(this->m_pin, LOW); 
+}
