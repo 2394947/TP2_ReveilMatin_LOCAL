38e9d9d21dc0e14387746a8070b87a68462d902d
2394947 <Jennifer.m1999@outlook.fr>
2025-01-02 15:40:02 -0500

    oops

--- AMOC_TP2_Reveil_Matin/include/NTPClock.h
+++ AMOC_TP2_Reveil_Matin/include/NTPClock.h
@@ -1,13 +1,13 @@
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 
-class RealTimeClock
+class NTPClock
 {
 private:
     WiFiUDP m_ntpUDP;
     NTPClient m_timeClient;
 
 public:
-    RealTimeClock(const char *p_serverRegion, long p_timezone, unsigned long p_updateInterval);
+    NTPClock(const char *p_serverRegion, long p_timezone, unsigned long p_updateInterval);
     NTPClient getTime();
 };
