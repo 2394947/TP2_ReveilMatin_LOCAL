a65a1ee7a3cd68aebb573ea5d4a7be9d503cbdf5
William <william.crepault@gmail.com>
2025-01-02 17:13:35 -0500

    retrait comms internalclock

--- AMOC_TP2_Reveil_Matin/src/InternalClock.cpp
+++ AMOC_TP2_Reveil_Matin/src/InternalClock.cpp
@@ -27,28 +27,3 @@ String InternalClock::getTime() {
 void InternalClock::tick() {
     // Work in progress ;)
 }
-
-
-
-// class InternalClock {
-// private:
-//     NTPClock* m_NTPClock;
-//     uint16_t m_ntpTime;
-//     uint16_t m_currentTime;
-//     uint8_t m_hours;
-//     uint8_t m_minutes;
-//     uint16_t m_secondes;
-//     uint16_t m_lastUpdate;
-//     uint16_t m_intervalUpdate;
-//     void computeTime();
-
-// public:
-//     InternalClock(
-//         NTPClock* p_NTPClock, 
-//         uint16_t p_intervalUpdate
-//         );
-//     String getTime();
-//     void tick();
-// };
-
-
