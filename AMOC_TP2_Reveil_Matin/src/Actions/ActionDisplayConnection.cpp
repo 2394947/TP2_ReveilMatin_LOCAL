549241547cedec556e62376a1d48759fe18365f3
2394947 <Jennifer.m1999@outlook.fr>
2025-01-02 15:02:55 -0500

    Supression de code inutile dans ActionDisplayConnection (loop inutile)

--- AMOC_TP2_Reveil_Matin/src/Actions/ActionDisplayConnection.cpp
+++ AMOC_TP2_Reveil_Matin/src/Actions/ActionDisplayConnection.cpp
@@ -1,20 +1,14 @@
 #include "Actions/ActionDisplayConnection.h"
 #include "config.h"
 
-ActionDisplayConnection::ActionDisplayConnection(Display4Digits* p_display4Digits, int p_interval)
+ActionDisplayConnection::ActionDisplayConnection(Display4Digits* p_display4Digits)
     :  m_display4Digits(p_display4Digits),
-       m_interval(p_interval),
        m_state(SHOW_D) { 
-        this->m_lastUpdateTime = 0;
+        ;
 }
 
 void ActionDisplayConnection::displayConnectionAnimation() {
-    // unsigned long currentTime = millis();
-
-    // if (currentTime - m_lastUpdateTime >= m_interval) {
-    //     m_lastUpdateTime = currentTime;
-
-        // Gestion des animations avant la connexion
+        
         switch (m_state) {
             case SHOW_D:
                 this->m_display4Digits->display(0,0,0,0); // Segment d
@@ -29,7 +23,6 @@ void ActionDisplayConnection::displayConnectionAnimation() {
                 m_state = SHOW_D;
                 break;
         }
-    //}
 }
 
 
