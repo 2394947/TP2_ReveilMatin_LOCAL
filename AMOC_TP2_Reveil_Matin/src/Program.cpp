c51b50220a8247920a30050278f590188d9bbd7e
2394947 <Jennifer.m1999@outlook.fr>
2025-01-03 12:58:51 -0500

    correction program.cpp

--- AMOC_TP2_Reveil_Matin/src/Program.cpp
+++ AMOC_TP2_Reveil_Matin/src/Program.cpp
@@ -15,6 +15,9 @@ Program::Program()
     Display4Digits* p_display4Digits = new Display4Digits(p_proxy);
     this->m_display4Digits = p_display4Digits;
     this->m_actionDisplayConnection = new ActionDisplayConnection(this->m_display4Digits);
+    this->m_connection = new Connection(INTERVAL_5,
+                                        this->m_actionDisplayConnection);
+    this->m_connection->connectToNetwork();
     this->m_ntpClock = new NTPClock(NTP_SERVER_REGION,
                                     TIMEZONE, UPDATE_INTERVAL
                                    );
@@ -25,13 +28,10 @@ Program::Program()
                                         this->m_buttonDisplayAlarms
                                        ,this->m_internalClock
                                        );
-    this->m_connection = new Connection(INTERVAL_5,
-                                        this->m_actionDisplayConnection);
-    this->m_connection->connectToNetwork();
 }
 
 void Program::loop() {
     this->m_connection->tick();
+    this->m_internalClock->tick();
     this->m_alarmClock->run();
-    // this->m_ntpClock->getTime();
 }
