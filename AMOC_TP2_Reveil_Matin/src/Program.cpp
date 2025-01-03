ce31b28b79e554b874209aa34e8ef112215d059d
2394947 <Jennifer.m1999@outlook.fr>
2025-01-03 12:22:13 -0500

    correction dans program.cpp

--- AMOC_TP2_Reveil_Matin/src/Program.cpp
+++ AMOC_TP2_Reveil_Matin/src/Program.cpp
@@ -6,8 +6,7 @@ uint8_t pinButtonAlarms = 2;
 
 Program::Program()
     : m_connection(nullptr),
-      m_ntpClock(nullptr)
-{
+      m_ntpClock(nullptr) {
     Display4DigitsProxy* p_proxy = new Display4DigitsProxyTM1637(
     CLK,
     DIO
@@ -16,16 +15,22 @@ Program::Program()
     Display4Digits* p_display4Digits = new Display4Digits(p_proxy);
     this->m_display4Digits = p_display4Digits;
     this->m_actionDisplayConnection = new ActionDisplayConnection(this->m_display4Digits);
-    this->m_ntpClock = new NTPClock(NTP_SERVER_REGION, TIMEZONE, UPDATE_INTERVAL);
-    this->m_internalClock = new InternalClock(this->m_ntpClock,INTERVAL_1);
+    this->m_ntpClock = new NTPClock(NTP_SERVER_REGION,
+                                    TIMEZONE, UPDATE_INTERVAL
+                                   );
+    this->m_internalClock = new InternalClock(this->m_ntpClock,
+                                              INTERVAL_1);
     this->m_buttonDisplayAlarms = new ButtonDisplayAvailableAlarm(pinButtonAlarms,500);
-    this->m_alarmClock = new AlarmClock(this->m_display4Digits,this->m_buttonDisplayAlarms,this->m_internalClock);
-    this->m_connection = new Connection(INTERVAL_5,this->m_actionDisplayConnection);
+    this->m_alarmClock = new AlarmClock(this->m_display4Digits,
+                                        this->m_buttonDisplayAlarms
+                                       ,this->m_internalClock
+                                       );
+    this->m_connection = new Connection(INTERVAL_5,
+                                        this->m_actionDisplayConnection);
     this->m_connection->connectToNetwork();
 }
 
-void Program::loop()
-{
+void Program::loop() {
     this->m_connection->tick();
     this->m_alarmClock->run();
     // this->m_ntpClock->getTime();
