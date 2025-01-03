9107cb0aea790ecdbb85da319d5fe7eee58b805d
2394947 <Jennifer.m1999@outlook.fr>
2025-01-03 12:19:19 -0500

    PetitesCorrections et ajout des membres dans program

--- AMOC_TP2_Reveil_Matin/include/Program.h
+++ AMOC_TP2_Reveil_Matin/include/Program.h
@@ -1,5 +1,8 @@
 #include "Connection.h"
 #include "NTPClock.h"
+#include "InternalClock.h"
+#include "AlarmClock.h"
+#include "Buttons/ButtonDisplayAvailableAlarm.h"
 #include "Actions/ActionDisplayConnection.h"
 #include "FourDigits/Display4Digits.h"
 #include "FourDigits/Display4DigitsProxyTM1637.h"
@@ -8,7 +11,10 @@ class Program
 public:
     Display4Digits *m_display4Digits;
     Connection *m_connection;
-    NTPClock *m_realTimeClock;
+    NTPClock *m_ntpClock;
+    InternalClock* m_internalClock;
+    AlarmClock* m_alarmClock;
+    ButtonDisplayAvailableAlarm* m_buttonDisplayAlarms;
     ActionDisplayConnection *m_actionDisplayConnection;
     Program();
     void loop();
