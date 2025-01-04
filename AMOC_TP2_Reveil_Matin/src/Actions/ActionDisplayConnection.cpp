3b7704c452698ee4c80da02cf44b88e1c501c26f
William <william.crepault@gmail.com>
2025-01-03 20:36:48 -0500

    correction ActionDisplayConnection pour suivrer tableau dans constantes

--- AMOC_TP2_Reveil_Matin/src/Actions/ActionDisplayConnection.cpp
+++ AMOC_TP2_Reveil_Matin/src/Actions/ActionDisplayConnection.cpp
@@ -7,22 +7,19 @@ ActionDisplayConnection::ActionDisplayConnection(Display4Digits* p_display4Digit
         ;
 }
 
-void ActionDisplayConnection::execute() {
-        
-        switch (m_state) {
-            case SHOW_D:
-                this->m_display4Digits->display(TAB_SEG_D, TAB_SEG_D, TAB_SEG_D, TAB_SEG_D); // Segment d
-               m_state = SHOW_G;
-                break;
-            case SHOW_G:
-                this->m_display4Digits->display(TAB_SEG_G, TAB_SEG_G, TAB_SEG_G, TAB_SEG_G); // Segment g
-                m_state = SHOW_A;
-                break;
-            case SHOW_A:
-                this->m_display4Digits->display(TAB_SEG_A, TAB_SEG_A, TAB_SEG_A, TAB_SEG_A); // Segment a
-                m_state = SHOW_D;
-                break;
-        }
+void ActionDisplayConnection::execute() {       
+    switch (m_state) {
+        case SHOW_D:
+            this->m_display4Digits->display(ARRAY_SEG_D, ARRAY_SEG_D, ARRAY_SEG_D, ARRAY_SEG_D); // Segment d
+            m_state = SHOW_G;
+            break;
+        case SHOW_G:
+            this->m_display4Digits->display(ARRAY_SEG_G, ARRAY_SEG_G, ARRAY_SEG_G, ARRAY_SEG_G); // Segment g
+            m_state = SHOW_A;
+            break;
+        case SHOW_A:
+            this->m_display4Digits->display(ARRAY_SEG_A, ARRAY_SEG_A, ARRAY_SEG_A, ARRAY_SEG_A); // Segment a
+            m_state = SHOW_D;
+            break;
+    }
 }
-
-
